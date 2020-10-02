5e622c72e450b99d99e4caa77faf4f2ce3b7e533
Allison Castaneda <allraec@yahoo.com>
2020-10-01 20:38:44 -0500

    5 & 6

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-
+//1 & 2
 document.querySelectorAll('.nav-link').forEach(link => {
     link.addEventListener('mouseover' , (e) => {
         e.target.style.color = 'red';
@@ -10,3 +10,26 @@ document.querySelectorAll('.nav-link').forEach(link => {
         e.target.style.color = '#212529';
     });
 });
+
+//3
+const homeLink = document.querySelector(".nav-link");
+window.addEventListener("load", () => {
+    homeLink.textContent = "Stay Home";
+});
+
+//4
+const header = document.querySelector('.main-navigation');
+header.addEventListener("click", (e) => {
+    e.target.style.backgroundColor = "olive";
+});
+
+//5 & 6
+const headerImg = document.querySelector('.intro img');
+headerImg.addEventListener("dblclick", (e) => {
+    e.target.style.transform = "scale(3)";
+});
+
+headerImg.addEventListener("mouseleave", (e) => {
+    e.target.style.transform = "scale(1)";
+})
+
