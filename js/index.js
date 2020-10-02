a9f6df60057a2b09c8a8df3160b5dd6692974977
Allison Castaneda <allraec@yahoo.com>
2020-10-01 19:08:54 -0500

    1-2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('mouseover' , (e) => {
+        e.target.style.color = 'red';
+    });
+
+    link.addEventListener('mouseout' , (e) => {
+        e.target.style.color = '#212529';
+    });
+});
